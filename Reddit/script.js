e255eb6faa92e5bde69d2e8ddb8c71aafc202890
Darya Ulyanova <dashaorluk@gmail.com>
2018-05-01 21:17:54 +0300

    change in stucture of code

--- Reddit/script.js
+++ Reddit/script.js
@@ -1,40 +1,70 @@
-//$(document).ready(function () {
-
-    var ajaxhttp = new XMLHttpRequest();
-    var url = "https://www.reddit.com/r/pics";
-    ajaxhttp.open("GET", url, true);
-    ajaxhttp.setRequestHeader("content-type", "application/json");
-    ajaxhttp.onreadystatechange = function () {
-        if(ajaxhttp.readyState == 4 && ajaxhttp.status == 200){
-            var jcontent = ajaxhttp;
-            console.log(jcontent);
-            console.log(ajaxhttp);
-        };
-    };
-    
-ajaxhttp.send();
-    //    $('.add-img').click(function () {
-    //        // add loading image to div
-    //        $('.loading').html('<img src="http://preloaders.net/preloaders/287/Filling%20broken%20ring.gif"> loading...');
-    //    });
-    //    // run ajax request
-    //    $.ajax({
-    //        crossDomain: true,
-    //        async: true,
-    //        type: 'GET',
-    //        dataType: 'json',
-    //        url: 'https://api.github.com/users/tarjalorem',
-    //        success: function (data) {
-    //            // replace div's content with returned data
-    //            setTimeout(function () {
-    //                $('.loading').html('<img src="' + data + '">');
-    //            }, 2000);
-    //        },
-    //        error: function() {
-    //            console.log('Error');
-    //        }
-    //    });
-    //    
-    //    
-
-//});
+       
+var get = function (url, callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+ 
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState != xhr.DONE) return;
+ 
+                var status = xhr.status;
+                var headers = xhr.getAllResponseHeaders();
+                var text = xhr.responseText;
+ 
+                callback(status, headers, text);
+            }
+ 
+            xhr.send();
+        }
+ 
+        var appendImage = function (url) {
+            var imgEl = document.createElement('img');
+            // <img />
+ 
+            imgEl.src = url;
+            // <img src="{url}" />
+ 
+            imgEl.onerror = function () {
+                // when image loading failed
+                imgEl.style.display = 'none';
+                // @todo hide image
+//                alert(1);
+            }
+ 
+            document.getElementById('images').appendChild(imgEl);
+        }
+ 
+        //getImages({limit: 5})
+        // getImages({})
+        // getImages() -- by default should take 100 images
+ 
+        // getImages({limit: 5, category: "cats"})
+        // getImages({category: "cats"})
+        // getImages()
+ 
+        // "S"OLID, S -> Single Responsibility
+        var getImages = function (limit) {
+            var arr = [];
+            var limit =5 ;
+             var url = 'https://www.reddit.com/r/pics/search.json?q=cats';                
+//                arr.push(url);
+//                console.log(arr)           
+//            var url = 'https://www.reddit.com/r/pics/search.json?q=cats';
+//            url.push(arr);
+//                    url += '&limit=5';
+                    // @todo: use here params.limit
+                    // @todo: category == "pics"
+ 
+            get(url, function (status, headers, body) {
+                var response = JSON.parse(body);
+ 
+                _.each(response.data.children, function (child) {
+                    var url = child.data.url;
+ 
+                    appendImage(url);
+ 
+                    console.log('ITEM!', child.data.url);
+                });
+ 
+            });
+        }
+        
